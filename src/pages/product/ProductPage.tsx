b1860aa13de646a6275670a69b020352955c23dc
RandimaAbhayawardhana <tharusharandima1@gmail.com>
2026-06-09 17:08:25 +0530

    Product page edited

--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { RefreshCw, Box, BarChart3, Binary, Layers, Sliders, Shield, Zap, Server, Network, ArrowRight } from 'lucide-react';
 import { Navbar } from "@/components/layout/Navbar"
 import { Footer } from "@/components/layout/Footer"
